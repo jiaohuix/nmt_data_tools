aad79255b8172825919e252c0288dd204303d833
MiuGod0126 <68314259+MiuGod0126@users.noreply.github.com>
2022-10-04 22:00:40 +0800

    Update run_mrasp_preprocess.sh

--- ras_sample/run_mrasp_preprocess.sh
+++ ras_sample/run_mrasp_preprocess.sh
@@ -9,6 +9,13 @@ bpe_code=$6
 repeat=$7
 prob=$8
 vocab_size=$9
+use_moses=$10
+if [ $use_moses == 1 ];
+  then
+    use_moses="--moses-detok"
+  else
+    use_moses=""
+fi
 
 rm -rf $out_folder
 
@@ -16,11 +23,19 @@ echo "--------------------Process bidirect and monolingual data.----------------
 bash ras_sample/preprocess_mrasp_raw.sh $in_folder $out_folder $src $tgt
 
 echo "--------------------Process Random Align Substitute data.--------------------"
-#for prefix in train valid
-for prefix in train valid
+# only valid for train set
+
+python my_tools/replace_word_bilingual.py --langs ${src},${tgt} --dict-path $dict_path \
+       --data-path $out_folder --prefix $prefix --num-repeat $repeat --moses-detok --replace-prob $prob \
+       --vocab-size $vocab_size
+
+bash ras_sample/merge_raw_ras.sh  $out_folder $prefix  $bpe_code $src $tgt
+
+
+for prefix in train
   do
     python my_tools/replace_word_bilingual.py --langs ${src},${tgt} --dict-path $dict_path \
-           --data-path $out_folder --prefix $prefix --num-repeat $repeat --moses-detok --replace-prob $prob \
+           --data-path $out_folder --prefix $prefix --num-repeat $repeat  $use_moses --replace-prob $prob \
            --vocab-size $vocab_size
     bash ras_sample/merge_raw_ras.sh  $out_folder $prefix  $bpe_code $src $tgt
 
